b9ffecf130bf3e96b334ad36fc3abcbf396d8bf7
Harish Vitta <harish_harish80@yahoo.com>
2016-04-13 00:28:50 -0400

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Harish Vitta on 13/4/16.
+//  Copyright © 2016 Harish Vitta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
